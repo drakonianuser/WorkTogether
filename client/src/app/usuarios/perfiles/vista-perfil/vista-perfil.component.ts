3529531af56d06499611c821eec7b5bc687899fd
Brandon Carmona <brandonvasco003@gmail.com>
2019-07-07 17:12:46 -0500

    perfil

--- client/src/app/usuarios/perfiles/vista-perfil/vista-perfil.component.ts
+++ client/src/app/usuarios/perfiles/vista-perfil/vista-perfil.component.ts
@@ -21,5 +21,11 @@ export class VistaPerfilComponent implements OnInit {
       }
     )
   }
+  foros:Array<any>=[
+    {nombreProyecto:'Work Together',descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis blanditiis eius itaque! Dolorum, inventore voluptas omnis esse suscipit culpa reiciendis officia veniam sed sunt expedita quam iusto? Natus, corrupti. Earum!'},
+    {nombreProyecto:'Work Together',descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis blanditiis eius itaque! Dolorum, inventore voluptas omnis esse suscipit culpa reiciendis officia veniam sed sunt expedita quam iusto? Natus, corrupti. Earum!'},
+    {nombreProyecto:'Work Together',descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis blanditiis eius itaque! Dolorum, inventore voluptas omnis esse suscipit culpa reiciendis officia veniam sed sunt expedita quam iusto? Natus, corrupti. Earum!'}
+    
+  ] 
 
 }
